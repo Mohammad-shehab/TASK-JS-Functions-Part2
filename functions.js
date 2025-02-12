d5fe930ce1d8dc1780345072400e479bec977166
Mohammad-shehab <your.q8mlg@outlook.com>
2025-02-12 11:36:43 +0300

    hdhdkjd

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+   console.log(`Hello ${name}`);
 }
 
 /**
@@ -20,7 +20,12 @@ function greet(name) {
  * isOdd(10) -> false
  */
 function isOdd(n) {
-  // Your code here
+if(n%2!=0){
+return true
+}
+else
+return false
+
 }
 
 /**
@@ -33,7 +38,8 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+  
+  return Math.floor(n / 2);
 }
 
 /**
@@ -47,7 +53,11 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
+if (n%2==0){
+
+return n+n}
+else
+return n*n
 }
 
 module.exports = {
